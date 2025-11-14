df746a0cf8e24029daa03bd7c1f59438830f86f7
SebastianThomas <kontakt@sthomas.ch>
2025-11-14 04:12:44 +0100

    JSON default

---
+++ std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/config/WebConfig.java
@@ -0,0 +1,18 @@
+package ch.sthomas.stddivelogger.ws.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
+        configurer
+                .defaultContentType(MediaType.APPLICATION_JSON) // default to JSON
+                .favorParameter(false)
+                .ignoreAcceptHeader(false);
+    }
+}
