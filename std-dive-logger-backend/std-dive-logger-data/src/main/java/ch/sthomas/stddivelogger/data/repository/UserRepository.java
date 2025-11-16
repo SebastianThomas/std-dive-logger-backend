9ab0e3775bcf973b589f1781c60bebd3f195c82e
SebastianThomas <kontakt@sthomas.ch>
2025-11-16 18:03:20 +0100

    Remove stale method on users

--- std-dive-logger-backend/std-dive-logger-data/src/main/java/ch/sthomas/stddivelogger/data/repository/UserRepository.java
+++ std-dive-logger-backend/std-dive-logger-data/src/main/java/ch/sthomas/stddivelogger/data/repository/UserRepository.java
@@ -12,10 +12,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     @Query(
-            "SELECT u FROM UserEntity u JOIN DiveBuddyEntity d ON d.user.id = u.id AND d.dive.id = :diveId")
-    List<UserEntity> findBuddies(long diveId);
-
-    @Query(
             value =
                     "SELECT u.* FROM t_users u INNER JOIN t_dive_privileges p ON u.pk_user_id = p.fk_user_id AND p.fk_dive_id = :diveId",
             nativeQuery = true)
