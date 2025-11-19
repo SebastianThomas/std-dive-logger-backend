1d790164bcbc857dbc70bf231a88dbcb66fd3a14
SebastianThomas <kontakt@sthomas.ch>
2025-11-19 13:25:30 +0100

    Add CORS COnfig

--- std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/config/WsMvcConfig.java
+++ std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/config/WsMvcConfig.java
@@ -3,14 +3,23 @@ package ch.sthomas.stddivelogger.ws.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WsMvcConfig implements WebMvcConfigurer {
 
     @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.defaultContentType(
-                MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.ALL);
+    public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
+        configurer
+                .defaultContentType(MediaType.APPLICATION_JSON) // default to JSON
+                .favorParameter(false)
+                .ignoreAcceptHeader(true);
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*.webdev-25.ivia.isginf.ch", "*.sthomas.ch", "localhost:*");
     }
 }
