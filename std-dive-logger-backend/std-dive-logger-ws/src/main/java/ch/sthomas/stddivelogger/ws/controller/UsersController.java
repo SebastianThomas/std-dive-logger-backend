9cbafc891364d11f45cc41fc567f7573e67a4cb0
SebastianThomas <kontakt@sthomas.ch>
2025-11-26 17:55:52 +0100

    Add Users Controller

---
+++ std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/controller/UsersController.java
@@ -0,0 +1,40 @@
+package ch.sthomas.stddivelogger.ws.controller;
+
+import ch.sthomas.stddivelogger.model.exception.UnauthorizedException;
+import ch.sthomas.stddivelogger.model.user.FrontendUser;
+import ch.sthomas.stddivelogger.model.user.User;
+import ch.sthomas.stddivelogger.service.UserService;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UsersController {
+
+    private final UserService userService;
+
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public FrontendUser loggedInUser(@AuthenticationPrincipal final User user) {
+        if (user == null) {
+            throw new UnauthorizedException("You must be logged in to access users.");
+        }
+        return user.toFrontendModel();
+    }
+
+    @GetMapping("/{id}")
+    public FrontendUser user(
+            @AuthenticationPrincipal final User user, @PathVariable final long id) {
+        if (user == null) {
+            throw new UnauthorizedException("You must be logged in to access users.");
+        }
+        return userService.getUserById(id).toFrontendModel();
+    }
+}
