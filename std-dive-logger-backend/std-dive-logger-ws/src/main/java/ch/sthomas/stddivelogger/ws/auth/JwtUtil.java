4ba99733bac1dffb739cd505a890a48e605aaaa6
SebastianThomas <kontakt@sthomas.ch>
2025-12-01 16:21:23 +0100

    Enable transaction on delete refresh token

--- std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/auth/JwtUtil.java
+++ std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/auth/JwtUtil.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.OffsetDateTime;
 import java.util.Date;
@@ -112,6 +113,7 @@ public class JwtUtil {
         };
     }
 
+    @Transactional
     public void deleteRefreshToken(final String refreshToken) {
         final var jti = extractJtiFromRefreshToken(refreshToken);
         refreshTokenRepository.deleteByJti(jti);
