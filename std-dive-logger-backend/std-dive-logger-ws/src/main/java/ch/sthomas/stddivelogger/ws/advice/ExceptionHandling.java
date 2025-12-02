cf3962a38e44f99ee01373977ae3716bef4eb140
SebastianThomas <kontakt@sthomas.ch>
2025-12-02 23:01:08 +0100

    Violations

--- std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/advice/ExceptionHandling.java
+++ std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/advice/ExceptionHandling.java
@@ -3,7 +3,12 @@ package ch.sthomas.stddivelogger.ws.advice;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.zalando.problem.spring.web.advice.ProblemHandling;
 import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
+import org.zalando.problem.spring.web.advice.validation.ConstraintViolationAdviceTrait;
 
 @RestControllerAdvice
 public class ExceptionHandling
-        implements ProblemHandling, SecurityAdviceTrait, NoSuchElementAdviceTrait {}
+        implements ProblemHandling,
+                SecurityAdviceTrait,
+                NoSuchElementAdviceTrait,
+                ConstraintViolationAdviceTrait,
+                DiveConstraintAdviceTrait {}
