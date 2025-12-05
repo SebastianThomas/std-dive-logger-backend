fa62ba6bbffd072ba4e48787d4228a2d793ed1b7
SebastianThomas <kontakt@sthomas.ch>
2025-12-05 09:53:50 +0100

    Search for users authenticated

--- std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/controller/UsersController.java
+++ std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/controller/UsersController.java
@@ -1,15 +1,13 @@
 package ch.sthomas.stddivelogger.ws.controller;
 
+import ch.sthomas.stddivelogger.data.model.PagedResponse;
 import ch.sthomas.stddivelogger.model.exception.UnauthorizedException;
 import ch.sthomas.stddivelogger.model.user.FrontendUser;
 import ch.sthomas.stddivelogger.model.user.User;
 import ch.sthomas.stddivelogger.service.UserService;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/users")
@@ -36,5 +34,16 @@ public class UsersController {
             throw new UnauthorizedException("You must be logged in to access users.");
         }
         return userService.getUserById(id).toFrontendModel();
+    }
+
+    @GetMapping("/search")
+    public PagedResponse<FrontendUser> user(
+            @AuthenticationPrincipal final User user,
+            @RequestParam(name = "query") final String query,
+            @RequestParam(name = "page", defaultValue = "0") final int page) {
+        if (user == null) {
+            throw new UnauthorizedException("You must be logged in to access users.");
+        }
+        return userService.getUsersByPartialName(query, page).map(User::toFrontendModel);
     }
 }
