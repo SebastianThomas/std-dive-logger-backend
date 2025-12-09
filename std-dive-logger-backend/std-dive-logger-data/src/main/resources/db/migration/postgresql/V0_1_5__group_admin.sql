78d5c175730908ec7abd94cf02a9d9120e73e3a4
SebastianThomas <kontakt@sthomas.ch>
2025-12-09 20:41:43 +0100

    Update readers to include check for groups, remove check for member for closest match

--- std-dive-logger-backend/std-dive-logger-data/src/main/resources/db/migration/postgresql/V0_1_5__group_admin.sql
+++ std-dive-logger-backend/std-dive-logger-data/src/main/resources/db/migration/postgresql/V0_1_5__group_admin.sql
@@ -1,4 +1,30 @@
 DELETE
 FROM t_group_member;
 ALTER TABLE t_group_member
-    ADD COLUMN role VARCHAR(7);
+    ADD COLUMN IF NOT EXISTS role VARCHAR(10);
+
+DROP VIEW IF EXISTS t_readers;
+-- READERS
+CREATE VIEW t_readers AS
+-- Diver
+SELECT d.pk_dive_id AS dive_id, u.*
+FROM t_users u
+         INNER JOIN t_dives d ON u.pk_user_id = d.fk_diver_id
+UNION
+-- Buddies (may include diver if >= 1 buddy
+SELECT d.pk_dive_id AS dive_id, u.*
+FROM t_dive_buddy b
+         INNER JOIN t_dives d
+                    ON b.fk_dive_id = d.pk_dive_id OR b.fk_buddy_dive_id = d.pk_dive_id
+         INNER JOIN t_users u ON d.fk_diver_id = u.pk_user_id
+UNION
+-- Explicit Readers
+SELECT p.fk_dive_id AS dive_id, u.*
+FROM t_users u
+         INNER JOIN t_dive_privileges p ON u.pk_user_id = p.fk_user_id
+UNION
+-- Group Readers
+SELECT g.fk_dive_id AS dive_id, u.*
+FROM t_users u
+         INNER JOIN t_group_member m ON u.pk_user_id = m.fk_user_id AND role IN ('MEMBER', 'ADMIN')
+         INNER JOIN t_dive_privileges_groups g ON g.fk_group_id = m.fk_group_id;
