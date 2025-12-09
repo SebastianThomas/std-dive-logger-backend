c9f8517b730c7a7892e13e1e569dd74cee792685
SebastianThomas <kontakt@sthomas.ch>
2025-12-09 19:15:13 +0100

    Add email profile for config

--- std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/config/EmailConfig.java
+++ std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/config/EmailConfig.java
@@ -6,10 +6,12 @@ import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
+@Profile("email")
 public class EmailConfig {
     @Bean
     public MailSender mailSender(
