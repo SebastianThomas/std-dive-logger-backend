6b230188404f6a03f3b8c50f2cea3d19527250fd
SebastianThomas <kontakt@sthomas.ch>
2025-12-10 09:31:29 +0100

    Add custom query for findAndDelete

--- std-dive-logger-backend/std-dive-logger-data/src/main/java/ch/sthomas/stddivelogger/data/repository/AccountRequestRepository.java
+++ std-dive-logger-backend/std-dive-logger-data/src/main/java/ch/sthomas/stddivelogger/data/repository/AccountRequestRepository.java
@@ -3,11 +3,22 @@ package ch.sthomas.stddivelogger.data.repository;
 import ch.sthomas.stddivelogger.model.entity.AccountRequestEntity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.OffsetDateTime;
 import java.util.Optional;
 
 public interface AccountRequestRepository extends JpaRepository<AccountRequestEntity, String> {
+    @Query(
+            value =
+                    """
+                            DELETE FROM t_account_request
+                            WHERE pk_account_request_id = :id
+                            RETURNING t_account_request.*
+                            """,
+            nativeQuery = true)
+    @Modifying
     Optional<AccountRequestEntity> findAndDeleteById(String id);
 
     void deleteAllByValidUntilBefore(OffsetDateTime validUntilBefore);
