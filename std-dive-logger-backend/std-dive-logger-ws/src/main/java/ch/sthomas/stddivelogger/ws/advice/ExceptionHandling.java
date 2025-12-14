36b21878cfd42e91e3db5c92ad2662ed5e5ccca6
SebastianThomas <kontakt@sthomas.ch>
2025-12-15 00:04:51 +0100

    Forbidden advice trait

--- std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/advice/ExceptionHandling.java
+++ std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/advice/ExceptionHandling.java
@@ -14,4 +14,5 @@ public class ExceptionHandling
                 ConstraintViolationAdviceTrait,
                 DiveConstraintAdviceTrait,
                 UnauthorizedAdviceTrait,
-                UserCreationAdviceTrait {}
+                UserCreationAdviceTrait,
+                ForbiddenAdviceTrait {}
