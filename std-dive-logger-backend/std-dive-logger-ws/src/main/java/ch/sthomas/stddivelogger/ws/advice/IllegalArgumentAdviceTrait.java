3b88edddba1f96364cfdab7e7a3307494903fdcb
SebastianThomas <kontakt@sthomas.ch>
2025-12-16 20:18:01 +0100

    MOre debug

--- std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/advice/IllegalArgumentAdviceTrait.java
+++ std-dive-logger-backend/std-dive-logger-ws/src/main/java/ch/sthomas/stddivelogger/ws/advice/IllegalArgumentAdviceTrait.java
@@ -1,5 +1,7 @@
 package ch.sthomas.stddivelogger.ws.advice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -8,9 +10,15 @@ import org.zalando.problem.Status;
 import org.zalando.problem.spring.web.advice.AdviceTrait;
 
 public interface IllegalArgumentAdviceTrait extends AdviceTrait {
+    Logger logger = LoggerFactory.getLogger(IllegalArgumentAdviceTrait.class);
+
     @ExceptionHandler
     default ResponseEntity<Problem> handleIllegalArgumentOperation(
             final IllegalArgumentException exception, final NativeWebRequest request) {
+        logger.warn(
+                "IllegalArgumentException while handling request to {}.",
+                request.getDescription(false),
+                exception);
         return create(Status.BAD_REQUEST, exception, request);
     }
 }
