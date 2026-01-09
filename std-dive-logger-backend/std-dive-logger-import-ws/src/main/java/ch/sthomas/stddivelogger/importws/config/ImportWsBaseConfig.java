fc2b0f8713968ea24d17f04a5ef7896ebb864953
SebastianThomas <kontakt@sthomas.ch>
2026-01-09 17:12:11 +0100

    Add correct banner

--- std-dive-logger-backend/std-dive-logger-import-ws/src/main/java/ch/sthomas/stddivelogger/importws/config/ImportWsBaseConfig.java
+++ std-dive-logger-backend/std-dive-logger-import-ws/src/main/java/ch/sthomas/stddivelogger/importws/config/ImportWsBaseConfig.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.zalando.problem.jackson.ProblemModule;
 import org.zalando.problem.violations.ConstraintViolationProblemModule;
@@ -22,6 +23,7 @@ import org.zalando.problem.violations.ConstraintViolationProblemModule;
 @EntityScan("ch.sthomas.stddivelogger.model.entity")
 @EnableJpaRepositories("ch.sthomas.stddivelogger.data.repository")
 @EnableTransactionManagement
+@EnableRetry
 public class ImportWsBaseConfig {
 
     static {
