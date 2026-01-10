41bc30ee9b5ae6681a2b558a1d7d42dd1c588fc7
SebastianThomas <kontakt@sthomas.ch>
2026-01-10 11:18:09 +0100

    Import WS CORS

--- std-dive-logger-backend/std-dive-logger-import-ws/src/main/java/ch/sthomas/stddivelogger/importws/config/ImportWsMvcConfig.java
+++ std-dive-logger-backend/std-dive-logger-import-ws/src/main/java/ch/sthomas/stddivelogger/importws/config/ImportWsMvcConfig.java
@@ -1,12 +1,22 @@
 package ch.sthomas.stddivelogger.importws.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ImportWsMvcConfig implements WebMvcConfigurer {
+
+    private final String[] allowedOrigins;
+
+    public ImportWsMvcConfig(
+            @Value("${ch.sthomas.stddivelogger.ws.cors.allowed_origins:}")
+                    final String[] allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
 
     @Override
     public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
@@ -14,5 +24,14 @@ public class ImportWsMvcConfig implements WebMvcConfigurer {
                 .defaultContentType(MediaType.APPLICATION_JSON) // default to JSON
                 .favorParameter(false)
                 .ignoreAcceptHeader(false);
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true) // For Cookies
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
